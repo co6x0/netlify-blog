0ae81d8e69b0843db8ad0dabfb89fa08d8978f26
Nao Komura <nao.komura@sixaxd.com>
2019-06-12 23:25:05 +0900

    gatsby tutorial2

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
+import dummyImg from "../assets/images/dummy_img-1.jpg"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: `purple` }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src={dummyImg} alt="" style={{ width: `320px` }} />
+  </div>
+)
