aea16fd4f9ab4d4a9d8fb929f10a835269b8f0c7
Nao Komura <nao.komura@sixaxd.com>
2020-02-16 19:04:53 +0900

    Update

--- co6ei-note/src/cms/cms.js
+++ co6ei-note/src/cms/cms.js
@@ -1,25 +1,24 @@
-// import CMS, { init } from 'netlify-cms-app'
 import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
-// if (process.env.NODE_ENV === 'development') {
-//   console.log('aaaaaaaaaaaa')
-//   const config = {
-//     backend: {
-//       name: 'git-gateway',
-//       branch: 'master'
-//     },
-//     collections: [
-//       {
-//         name: 'blog',
-//         label: 'Blog',
-//         folder: 'co6ei-note/src/pages/blog/test'
-//       }
-//     ]
-//   }
-//   init({ config })
-// }
+if (process.env.NODE_ENV === 'development') {
+  console.log('aaaaaaaaaaaa')
+  const config = {
+    backend: {
+      name: 'git-gateway',
+      branch: 'master'
+    },
+    collections: [
+      {
+        name: 'blog',
+        label: 'Blog',
+        folder: 'co6ei-note/src/pages/blog/test'
+      }
+    ]
+  }
+  CMS.init({ config })
+}
 
 // import BlogPostPreview from './preview-templates/BlogPostPreview'
 
