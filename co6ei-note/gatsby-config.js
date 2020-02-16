7921f9b02a9c5c522511303e3a5d4ee84ba9ae1f
Nao Komura <nao.komura@sixaxd.com>
2020-02-16 18:40:54 +0900

    Update

--- co6ei-note/gatsby-config.js
+++ co6ei-note/gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        manualInit: true,
+        // manualInit: true,
         modulePath:`${__dirname}/src/cms/cms.js`,
       },
     },
