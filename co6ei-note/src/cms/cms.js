d914e12c8afd5c55ec5f90de1c664b918db17247
Nao Komura <nao.komura@sixaxd.com>
2020-02-16 16:10:33 +0900

    Update production branch, temporary

--- co6ei-note/src/cms/cms.js
+++ co6ei-note/src/cms/cms.js
@@ -1,6 +1,8 @@
-import CMS from 'netlify-cms-app'
+import CMS, { init } from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
+
+const { GATSBY_CMS_BRANCH } = process.env
 
 // import BlogPostPreview from './preview-templates/BlogPostPreview'
 
